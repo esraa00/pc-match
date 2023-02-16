bd431948f66ba48d3d88213090a06ccecabc6bea
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-02-16 11:37:09 +0200

    added user controller

---
+++ src/user/user.controller.ts
@@ -0,0 +1,6 @@
+import { Controller } from '@nestjs/common';
+import { UserService } from './user.service';
+@Controller('user')
+export class UserController {
+  constructor(private userService: UserService) {}
+}
