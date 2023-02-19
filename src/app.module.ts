0bbe1f91e0d0c507cd3aa958180c857aca2bd3bb
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-02-19 16:53:39 +0200

    managed app configuration and database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+//for root => automatically be shared down into all other modules
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'postgres',
+          username: config.get<string>('DB_USERNAME'),
+          database: config.get<string>('DB_NAME'),
+          password: config.get<string>('DB_PASSWORD'),
+          synchronize: true,
+          entities: [],
+        };
+      },
+    }),
+    UserModule,
+  ],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
