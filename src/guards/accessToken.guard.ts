ad14d1e7cd1da75c890af445219e8997a1a2c4a8
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-02-27 21:38:30 +0200

    updated logic for exports

--- src/guards/accessToken.guard.ts
+++ src/guards/accessToken.guard.ts
@@ -1,4 +1,8 @@
 import { AuthGuard } from '@nestjs/passport';
+import { UseGuards } from '@nestjs/common';
+export function UseAccessTokenGuard() {
+  return UseGuards(AccessTokenGuard);
+}
 
 export class AccessTokenGuard extends AuthGuard('jwt') {
   constructor() {
--- src/guards/refreshToken.guard.ts
+++ src/guards/refreshToken.guard.ts
@@ -1,4 +1,8 @@
 import { AuthGuard } from '@nestjs/passport';
+import { UseGuards } from '@nestjs/common';
+export function UseRefreshTokenGuard() {
+  return UseGuards(RefreshTokenGuard);
+}
 
 export class RefreshTokenGuard extends AuthGuard('jwt-refresh') {
   constructor() {
