d18e7a11d250a3c797b4e4627b5c9dfd1869ad81
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-02-28 04:06:09 +0200

    added routes for local login and sign up

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,76 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Res } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { AuthService } from './auth.service';
 import { LogInUserDto } from './dto/login-user.dto';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+import { GetCookies, GetCurrentUser } from 'src/decorators';
+import { UseAccessTokenGuard, UseRefreshTokenGuard } from 'src/guards';
+
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
-  @Post('/local/signup')
-  async signupLocal(@Body() body: CreateUserDTO) {
-    return await this.authService.signupLocal(body);
+  constructor(
+    private authService: AuthService,
+    private configService: ConfigService,
+  ) {}
+
+  @Post('local/signup')
+  // @UseInterceptors(new SetCookieInterceptor(['accessToken', 'refreshToken'], [{}, {}]))
+  async signupLocal(
+    @Body() body: CreateUserDTO,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { accessToken, refreshToken } = await this.authService.signupLocal(
+      body,
+    );
+    res.cookie('accessToken', accessToken, {
+      maxAge: +this.configService.get('COOKIE_ACCESS_TOKEN_EXP'),
+    });
+    res.cookie('refreshToken', refreshToken, {
+      maxAge: +this.configService.get('COOKIE_REFRESH_TOKEN_EXP'),
+    });
   }
 
-  @Post('/local/login')
-  async loginLocal(@Body() body: LogInUserDto) {
-    return await this.authService.loginLocal(body);
+  @Post('local/login')
+  async loginLocal(
+    @Body() body: LogInUserDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { accessToken, refreshToken } = await this.authService.loginLocal(
+      body,
+    );
+    res.cookie('accessToken', accessToken, {
+      maxAge: +this.configService.get('COOKIE_ACCESS_TOKEN_EXP'),
+    });
+    res.cookie('refreshToken', refreshToken, {
+      maxAge: +this.configService.get('COOKIE_REFRESH_TOKEN_EXP'),
+    });
+  }
+
+  @UseAccessTokenGuard()
+  @Post('logout')
+  async logout(
+    @GetCurrentUser('userId') userId: number,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    await this.authService.logout(userId);
+    res.cookie('accessToken', '');
+    res.cookie('refreshToken', '');
+  }
+
+  @UseRefreshTokenGuard()
+  @Post('refresh')
+  async refreshToken(
+    @GetCurrentUser('userId') userId: number,
+    @GetCookies('refreshToken') refreshToken: string,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const accessToken = await this.authService.refreshToken(
+      userId,
+      refreshToken,
+    );
+    res.cookie('accessToken', accessToken, {
+      maxAge: +this.configService.get('COOKIE_REFRESH_TOKEN_EXP'),
+    });
   }
 }
