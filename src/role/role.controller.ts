7b684bd75c9da4c1b2602ccede3121d35402f500
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-03-23 08:13:57 +0200

    implement create role

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, ConflictException } from '@nestjs/common';
+import { CreateRoleDTO } from './dto/create-role.dto';
+import { RoleService } from './role.service';
 
 @Controller('role')
-export class RoleController {}
+export class RoleController {
+  constructor(private readonly roleService: RoleService) {}
+
+  @Post()
+  async createRole(@Body() createRoleDTO: CreateRoleDTO) {
+    const isRoleFound = await this.roleService.findOneByName(
+      createRoleDTO.roleName,
+    );
+    if (isRoleFound) throw new ConflictException('role already exist');
+    return await this.roleService.create(createRoleDTO.roleName);
+  }
+}
