274bc164abe7db6c686f7b11129982a2a5eee1b9
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-03-23 06:45:46 +0200

    implemented get tag by name route

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get, Post, Body, ConflictException } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  ConflictException,
+  Param,
+} from '@nestjs/common';
+import { NotFoundException } from '@nestjs/common/exceptions';
 import { CreateTagDTO } from './dto/Create-tag.dto';
+import { GetTagByNameDTO } from './dto/get-tag-by-name.dto';
 import { TagService } from './tag.service';
 
 @Controller('tag')
@@ -13,5 +22,12 @@ export class TagController {
     );
     if (isTagFound) throw new ConflictException('tag already exist');
     return await this.tagService.create(createTagDTO.tagName);
+  }
+
+  @Get('/:tagName')
+  async getTagByName(@Param() getTagByNameDTO: GetTagByNameDTO) {
+    const tag = await this.tagService.findOneByName(getTagByNameDTO.tagName);
+    if (!tag) throw new NotFoundException('tag was not found');
+    return await this.tagService.findOneByName(getTagByNameDTO.tagName);
   }
 }
