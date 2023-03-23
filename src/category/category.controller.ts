85aa7fab465f402923747e851b19a5041c22028a
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-03-23 07:19:51 +0200

    implement create category route

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, ConflictException } from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { CreateCategoryDTO } from './dto/create-category.dto';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Post()
+  async createCategory(@Body() createCategoryDTO: CreateCategoryDTO) {
+    const isCategoryFound = await this.categoryService.findOneByName(
+      createCategoryDTO.categoryName,
+    );
+    if (isCategoryFound) throw new ConflictException('tag already exist');
+    return await this.categoryService.create(createCategoryDTO.categoryName);
+  }
+}
