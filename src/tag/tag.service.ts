5751b303f1d5b8855556985f69cd5a83ed6d25f9
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-03-23 06:06:04 +0200

    implement findOneByName service function

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Tag } from './tag.entity';
 
 @Injectable()
-export class TagService {}
+export class TagService {
+  constructor(@InjectRepository(Tag) private repo: Repository<Tag>) {}
+  async findOneByName(tagName: string) {
+    const tag = await this.repo.findOneBy({ tagName });
+    if (!tag) throw new NotFoundException('tag was not found');
+    return tag;
+  }
+}
