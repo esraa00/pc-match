e02b7846c9400acd24d2fc049ddc0d0d3a6a50bb
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-03-23 06:33:10 +0200

    implement create tag route

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, ConflictException } from '@nestjs/common';
+import { CreateTagDTO } from './dto/Create-tag.dto';
+import { TagService } from './tag.service';
 
 @Controller('tag')
-export class TagController {}
+export class TagController {
+  constructor(private readonly tagService: TagService) {}
+
+  @Post()
+  async createTag(@Body() createTagDTO: CreateTagDTO) {
+    const isTagFound = await this.tagService.findOneByName(
+      createTagDTO.tagName,
+    );
+    if (isTagFound) throw new ConflictException('tag already exist');
+    return await this.tagService.create(createTagDTO.tagName);
+  }
+}
