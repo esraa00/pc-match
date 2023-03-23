1fe794f641fbeb0c568fa0265f1688695e29b666
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-03-23 06:19:02 +0200

    fix findOneByName function logic

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Tag } from './tag.entity';
@@ -6,9 +6,8 @@ import { Tag } from './tag.entity';
 @Injectable()
 export class TagService {
   constructor(@InjectRepository(Tag) private repo: Repository<Tag>) {}
+
   async findOneByName(tagName: string) {
-    const tag = await this.repo.findOneBy({ tagName });
-    if (!tag) throw new NotFoundException('tag was not found');
-    return tag;
+    return await this.repo.findOneBy({ tagName });
   }
 }
